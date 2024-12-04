5d4bfa3e15f862a9e7f88c0b0f98214edd8d4479
PJutch <103033969+PJutch@users.noreply.github.com>
2024-12-04 23:59:29 +0300

    Refactor and implement proper deleting

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 import tkinter
 from tkinter import ttk
 
-
 entry_rows = []
+next_row_id = 0
 
 
-def remove_row(row_index):
-    for widget in entry_rows[row_index]:
-        widget.pack_forget()
-    del entry_rows[row_index]
+class EntryRow:
+    def __init__(self):
+        global next_row_id
+        self.id = next_row_id
+        next_row_id += 1
+
+        self.widgets = [ttk.Entry(frame),
+                        ttk.Button(frame, text="Удалить", command=lambda: remove_row(self.id))]
+
+    def forget(self):
+        for widget in self.widgets:
+            widget.grid_forget()
+
+    def grid(self, row):
+        for j in range(len(self.widgets)):
+            self.widgets[j].grid(row=row, column=j, sticky=tkinter.W + tkinter.E)
+
+
+def remove_row(deleted):
+    global entry_rows
+
+    for row in entry_rows:
+        row.forget()
+
+    entry_rows = [row for row in entry_rows if row.id != deleted]
+
+    for i in range(len(entry_rows)):
+        entry_rows[i].grid(i)
 
 
 def add_row():
-    entry_rows.append([ttk.Entry(frame),
-                       ttk.Button(frame, text="Удалить", command=lambda: None)])
+    global entry_rows
 
-    for i in range(len(entry_rows[-1])):
-        entry_rows[-1][i].grid(column=i, row=len(entry_rows) - 1, sticky=tkinter.W+tkinter.E)
+    for row in entry_rows:
+        row.forget()
 
-    add_button.pack_forget()
-    add_button.pack(anchor=tkinter.N, padx=6, pady=6)
+    entry_rows.append(EntryRow())
+
+    for i in range(len(entry_rows)):
+        entry_rows[i].grid(i)
 
 
 if __name__ == '__main__':
@@ -31,6 +56,6 @@ if __name__ == '__main__':
     frame.pack(fill=tkinter.X)
 
     add_button = ttk.Button(text="Добавить", command=add_row)
-    add_row()
+    add_button.pack(anchor=tkinter.N, padx=6, pady=6)
 
     window.mainloop()
