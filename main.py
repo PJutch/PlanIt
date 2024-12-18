eb21dc0ae3e294069683dab455692b6429340334
PJutch <103033969+PJutch@users.noreply.github.com>
2024-12-18 20:08:05 +0300

    Add App class

--- main.py
+++ main.py
@@ -5,19 +5,26 @@ import save
 from subjects import Subjects
 from tasks import Tasks
 
+
+class App:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.title('PlanIt')
+
+        notebook = ttk.Notebook()
+        notebook.pack(expand=True, fill=tkinter.BOTH)
+
+        self.subjects = Subjects(notebook)
+        self.tasks = Tasks(notebook)
+
+        self.subjects.tasks = self.tasks
+        self.tasks.subjects = self.subjects
+
+        save.load(self.subjects, self.tasks)
+
+    def run(self):
+        self.window.mainloop()
+
+
 if __name__ == '__main__':
-    window = tkinter.Tk()
-    window.title('PlanIt')
-
-    notebook = ttk.Notebook()
-    notebook.pack(expand=True, fill=tkinter.BOTH)
-
-    subjects = Subjects(notebook)
-    tasks = Tasks(notebook)
-
-    subjects.tasks = tasks
-    tasks.subjects = subjects
-
-    save.load(subjects, tasks)
-
-    window.mainloop()
+    App().run()
